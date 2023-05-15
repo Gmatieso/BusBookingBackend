e47ee8f8af9ee5008a45da26d25636c487c3775e
root <root@DESKTOP-UB6UJDP.localdomain>
2023-05-15 11:07:13 +0300

    Added new files

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  resources :routes
+  resources :bookings
+  resources :buses
+  # resources :clients
+  # resources :drivers
+  # resources :admins
+
+  post '/admin-signup', to: 'admins#signup'
+  get '/admin-me', to: 'admins#show'
+  post '/admin-login', to: 'admins#login'
+  delete '/admin-logout', to: 'admins#logout'
+
+  post '/client-signup', to: 'clients#signup'
+  get '/client-me', to: 'clients#show'
+  post '/client-login', to: 'clients#login'
+  delete '/client-logout', to: 'clients#logout'
+
+  post '/driver-signup', to: 'drivers#signup'
+  get '/driver-me', to: 'drivers#show'
+  post '/driver-login', to: 'drivers#login'
+  delete '/driver-logout', to: 'drivers#logout'
+end
